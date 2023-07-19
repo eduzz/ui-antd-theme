5ca2513ee7dfab489213b90be554e4baca07666d
danieloprado <danielprado.ad@gmail.com>
2023-07-19 11:47:25 -0300

    feat: fix reset

--- CssReset.tsx
+++ CssReset.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from 'react';
 import './reset.css';
 
-const CssReset = () => null;
+const CssReset = () => {
+  useEffect(() => {
+    const elems = document.querySelectorAll('html,body');
+    elems.forEach(el => el.classList.add('--eduzz-ui-antd-theme-reset'));
+
+    return () => {
+      elems.forEach(el => el.classList.remove('--eduzz-ui-antd-theme-reset'));
+    };
+  }, []);
+
+  return null;
+};
 
 export default CssReset;
